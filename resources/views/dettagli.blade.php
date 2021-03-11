2c2d974d63a24e3336a560cadc67ccada7f7f99d
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-11 17:06:21 +0100

    Details stamp ok

--- resources/views/dettagli.blade.php
+++ resources/views/dettagli.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('title', $formato['titolo'])
 
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+@endsection
+
 @section('content')
 
 <div class="prodotto">
@@ -14,6 +18,21 @@
   <div class="descrizione">
     <div class="container">
       {!!$formato['descrizione']!!}
+      <div class="info">
+        <div class="box-info">
+          <i class="fas fa-info"></i>
+          <p>{!!$formato['tipo']!!}</p>
+        </div>
+        <div class="box-info">
+            <i class="far fa-clock"></i>
+            <p>{!!$formato['cottura']!!}</p>
+        </div>
+        <div class="box-info">
+          <i class="fas fa-balance-scale-right"></i>
+          <p>{!!$formato['peso']!!}</p>
+        </div>
+
+      </div>
     </div>
   </div>
 </div>
